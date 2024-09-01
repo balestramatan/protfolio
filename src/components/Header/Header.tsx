af746df351ad39b3cfcca46019b720427a5c8eb4
Matan Balestra <matan.balestra@gmail.com>
2024-09-01 20:08:53 +0300

    capital letter

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
-import './header.css';
+import './Header.css';
 
 type HeaderProps = {
   onLanguageChange: (lang: string) => void;
