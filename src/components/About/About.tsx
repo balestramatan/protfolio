c2ba31f37ffa6be2c754bbd357612dcc2337afda
Matan Balestra <matan.balestra@gmail.com>
2024-09-02 17:32:51 +0300

    add download button and add meta data

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 
 import cv from '../../assets/pdf_files/MatanCV.pdf';
 
+import { useTranslation } from 'react-i18next';
 import './About.css';
 
 const About: React.FC = () => {
+  const {t} = useTranslation();
   return (
-    // <section className="about-section">
-       <object data={cv} type="application/pdf" width="95%" height="100%" />
-    // </section>
+    <section className='about-section'>
+      <object data={cv} type="application/pdf" width="95%" height="95%" />
+
+      <div className="download-button">
+          <a href={cv} download="Matan_Balestra_Resume.pdf">
+            {t('downloadResume')}
+          </a>
+      </div>
+    </section>
   );
 };
 
