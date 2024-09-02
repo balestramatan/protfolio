ae35611646f0a7fb6ab42e46534134e7800c0aae
Matan Balestra <matan.balestra@gmail.com>
2024-09-02 16:34:47 +0300

    add not found page

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import About from './components/About/About';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import TabletOrMobileHeader from './components/Header/TabletOrMobileHeader';
 import Hero from './components/Hero/Hero';
+import NotFound from './components/NotFound/NotFound';
 import Projects from './components/Projects/Projects';
+import ToastComponent from './components/Toast/Toast';
+import useLanguage from './hooks/useLanguage';
 
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 import { useMediaQuery } from 'react-responsive';
+
 import './App.css';
-import TabletOrMobileHeader from './components/Header/TabletOrMobileHeader';
-import ToastComponent from './components/Toast/Toast';
-import useLanguage from './hooks/useLanguage';
 
 const App: React.FC = () => {
   const isDesktopOrLaptop = useMediaQuery({ query: '(min-width: 800px)' })
@@ -28,6 +30,7 @@ const App: React.FC = () => {
             <Route path="/about" element={<About />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
         <Footer />
