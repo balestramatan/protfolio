608a875b45692f6d37a325421f99e297b1db8e32
Matan Balestra <matan.balestra@gmail.com>
2024-09-02 15:50:27 +0300

    fix things

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,22 +1,14 @@
 import React from 'react';
-import './About.css';
 
-
-import { useTranslation } from 'react-i18next';
 import cv from '../../assets/pdf_files/MatanCV.pdf';
 
-const About: React.FC = () => {
-  const {t} = useTranslation();
-  return (
-    <section className="about-section">
-      <embed src={`${cv}#toolbar=0`} type="application/pdf" width="70%" height="600px" />
+import './About.css';
 
-      <div className="download-button">
-        <a href={cv} download="Matan_Balestra_Resume.pdf">
-          {t('downloadResume')}
-        </a>
-      </div>
-    </section>
+const About: React.FC = () => {
+  return (
+    // <section className="about-section">
+       <object data={cv} type="application/pdf" width="95%" height="100%" />
+    // </section>
   );
 };
 
